21be01efb76b1f77a6572bb9dbbf358193821642
claraasouzaw <maria.clara@estudante.ifro.edu.br>
2024-10-11 12:53:24 -0400

    Primeiro commit Clara

---
+++ iBeauty-main/Telas/CadastroCliente.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IBeauty.Telas
+{
+    /// <summary>
+    /// Lógica interna para CadastroCliente.xaml
+    /// </summary>
+    public partial class CadastroCliente : Window
+    {
+        private bool isAnamneseExpanded = false;
+        private bool isMenuExpanded = false;
+        public CadastroCliente()
+        {
+            InitializeComponent();
+        }
+        private void BtnToggleMenu_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(0.3))
+            };
+
+            if (isMenuExpanded)
+            {
+                animation.From = MenuPanel.ActualWidth;
+                animation.To = 0;
+
+                animation.Completed += (s, a) =>
+                {
+                    MenuPanel.Visibility = Visibility.Collapsed;
+                };
+            }
+            else
+            {
+                MenuPanel.Visibility = Visibility.Visible;
+
+                animation.From = 0;
+                animation.To = 250;
+            }
+
+            MenuPanel.BeginAnimation(FrameworkElement.WidthProperty, animation);
+            isMenuExpanded = !isMenuExpanded;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void BtnMenu_Click(object sender, RoutedEventArgs e)
+        {
+            TelaInicial inicio = new TelaInicial();
+            inicio.Show();
+            this.Close();
+        }
+        private void BtnAnamnese_Click(object sender, RoutedEventArgs e)
+        {
+            isAnamneseExpanded = !isAnamneseExpanded;
+            AnamneseOptions.Visibility = isAnamneseExpanded ? Visibility.Visible : Visibility.Collapsed;
+        }
+        private void BtnAnamneseCapilar_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroAnamneseCapilar CAC = new CadastroAnamneseCapilar();
+            CAC.Show();
+            this.Close();
+        }
+
+        private void BtnAnamneseCorporal_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroAnamneseCorporal CACO = new CadastroAnamneseCorporal();
+            CACO.Show();
+            this.Close();
+        }
+
+        private void BtnAnamneseFacial_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroAnamneseFacial CAF = new CadastroAnamneseFacial();
+            CAF.Show();
+            this.Close();
+        }
+
+        private void BtnAnamneseManicure_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroAnamneseManicurePedicure CAMP = new CadastroAnamneseManicurePedicure();
+            CAMP.Show();
+            this.Close();
+        }
+        private void BtnCadastroFuncionario_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroFuncionario CFU = new CadastroFuncionario();
+            CFU.Show();
+            this.Close();
+        }
+        private void BtnCadastroCliente_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        private void BtnCadastroExpediente_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroExpediente CEX = new CadastroExpediente();
+            CEX.Show();
+            this.Close();
+        }
+        private void BtnCadastroProduto_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroProduto CPR = new CadastroProduto();
+            CPR.Show();
+            this.Close();
+        }
+
+        private void BtnCadastroFornecedor_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroFornecedor CFOR = new CadastroFornecedor();
+            CFOR.Show();
+            this.Close();
+        }
+        private void BtnServico_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            Servico SE = new Servico();
+            SE.Show();
+            this.Close(); */
+        }
+
+    
+
+    private void BtnLimpar_Click(object sender, RoutedEventArgs e)
+        {//limpa os dados da parte de endereço
+            try
+            {
+                rua_txt.Text = "";
+                bairro_txt.Text = "";
+                numero_txt.Text = "";
+                complemento_txt.Text = "";
+                cidade_txt.Text = "";
+                estado_txt.Text = "";
+                cep_txt.Text = "";
+                numero_telefone_txt.Text = "";
+                user_txt.Text = "";
+                senha_txt.Text = "";
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
+
