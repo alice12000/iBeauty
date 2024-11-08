a6555cf63556844917a6cc49ca25c0f763cf8e99
dudastrelow <100968917+dudastrelow@users.noreply.github.com>
2024-11-08 13:18:12 -0400

    correção

--- Helpers/SessionHelper.cs
+++ Helpers/SessionHelper.cs
@@ -15,43 +15,43 @@ namespace IBeauty.Helpers
     {
         private static SessionHelper _instance;
 
-        private static Usuario _usuario = null;
+        //private static Usuario _usuario = null;
 
-        private SessionHelper() { }
+        //private SessionHelper() { }
 
-        public static SessionHelper GetInstance()
-        {
-            if (_instance == null)
-                _instance = new SessionHelper();
+        //public static SessionHelper GetInstance()
+        //{
+        //    if (_instance == null)
+        //        _instance = new SessionHelper();
 
-            return _instance;
-        }
+        //    return _instance;
+        //}
 
-        public static bool Login(string usuario, string senha)
-        {
-            _ = GetInstance();
+        //public static bool Login(string usuario, string senha)
+        //{
+        //    _ = GetInstance();
 
-            if (_usuario != null)
-                return true;
+        //    if (_usuario != null)
+        //        return true;
 
-            var user = new UsuarioDAO().GetByUsuario(usuario);
+        //    var user = new UsuarioDAO().GetByUsuario(usuario);
 
-            _usuario = user;
+        //    _usuario = user;
 
-            return HashHelper.Compare(senha, user.Senha);
-        }
+        //    return HashHelper.Compare(senha, user.Senha);
+        //}
 
-        public static void Logout()
-        {
-            _usuario = null;
-        }
+        //public static void Logout()
+        //{
+        //    _usuario = null;
+        //}
 
-        public static Usuario GetUsuario()
-        {
-            _ = GetInstance();
+        //public static Usuario GetUsuario()
+        //{
+        //    _ = GetInstance();
 
-            return _usuario != null ? _usuario : null;
-        }
+        //    return _usuario != null ? _usuario : null;
+        //}
 
 
     }
