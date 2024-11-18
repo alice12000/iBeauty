76ea26aeb205f5640cf716d20404857feb62697c
Helo <heloize.karoline@estudante.ifro.edu.br>
2024-11-17 21:51:12 -0400

    Ligação expediente

---
+++ Models/CadastroExpediente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBeauty.Models
+{
+    public class CadastroExpediente
+    {
+        public int Id { get; set; }
+        public string Mes { get; set; }
+        public string Nome { get; set; }
+        public string Ano { get; set; }
+        public string Carga_horaria { get; set; }
+        public string Salario { get; set; }
+
+        public CadastroExpediente(int id, string mes, string nome, string ano, string carga_horaria, string salario)
+        {
+            Id = id;
+            Mes = mes;
+            Nome = nome;
+            Ano = ano;
+            Carga_horaria = carga_horaria;
+            Salario = salario;
+         
+        }
+    }
+}
--- Telas/CadastroExpediente.xaml.cs
+++ Telas/CadastroExpediente.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using IBeauty.Database;
+using IBeauty.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace IBeauty.Telas
 {
     /// <summary>
@@ -24,5 +29,36 @@ namespace IBeauty.Telas
         {
             InitializeComponent();
         }
+
+        private void cargahoraria_txt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void mes_txt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void Cadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            string mes = tb_mes.Text;
+            string nome = tb_nome.Text;
+            string ano = tb_ano.Text;
+            string carga_horaria = tb_carga_horaria.Text;
+            string salario = tb_salario.Text;
+            int numero = 0;
+
+            var Conexao = new Conexao();
+
+            var Expediente = new CadastroExpediente(0, mes, nome, ano, carga_horaria, salario);
+
+
+        }
+
+        private void Voltar_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
     }
 }
