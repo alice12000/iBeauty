7677f46664e41e97b398168870b743bc8348ad32
dudastrelow <100968917+dudastrelow@users.noreply.github.com>
2024-11-24 15:44:30 -0400

    Cadastro de comanda sem bd

---
+++ Telas/CadastroComanda.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IBeauty.Telas
+{
+    /// <summary>
+    /// Interação lógica para CadastroComanda.xam
+    /// </summary>
+    public partial class CadastroComanda : Page
+    {
+        public CadastroComanda()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFechar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnServicoComanda_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- Telas/JanelaPrincipal.xaml.cs
+++ Telas/JanelaPrincipal.xaml.cs
@@ -9,7 +9,7 @@ namespace IBeauty
         public JanelaPrincipal()
         {
             InitializeComponent();
-            MainFrame.Navigate(new TelaInicial());
+            MainFrame.Navigate(new Teste());
 
             bool isUserLoggedIn = false;
 
