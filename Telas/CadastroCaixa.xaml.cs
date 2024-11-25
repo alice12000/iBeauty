2c20e125bf4829284cb655b97ca51512f14bc3dd
dudastrelow <100968917+dudastrelow@users.noreply.github.com>
2024-11-24 22:51:01 -0400

    Cadastro caixa sem bd

---
+++ Telas/CadastroCaixa.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IBeauty.Telas
+{
+    /// <summary>
+    /// Interação lógica para CadastroCaixa.xam
+    /// </summary>
+    public partial class CadastroCaixa : Page
+    {
+        public CadastroCaixa()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvarEntrada_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFecharEntrada_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEntrada_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSaida_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFecharSaida_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFecharCaixa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
